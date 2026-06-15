9f83854740c50c2a0e224c31e181d24cc5d4ec1c
AugustoBortoncello3547 <augusto.bortoncello@gmail.com>
2026-06-15 19:04:33 -0300

    fix: validator na edição de veículo

--- src/Veiculos/Contract/EditarVeiculoRequest.php
+++ src/Veiculos/Contract/EditarVeiculoRequest.php
@@ -6,6 +6,7 @@ namespace App\Veiculos\Contract;
 
 use App\Veiculos\Model\VeiculoModel;
 use App\Core\Contract\AbstractContract;
+use App\Veiculos\Validator\Placa;
 use Symfony\Component\Validator\Constraints as Assert;
 use OpenApi\Attributes as OA;
 
@@ -23,12 +24,7 @@ readonly class EditarVeiculoRequest extends AbstractContract {
     public static function getConstraints(): Assert\Collection {
         return new Assert\Collection([
             'placa' => [
-                new Assert\Optional(
-                    new Assert\Regex(
-                        pattern: '/^[A-Z]{3}-?\d{4}$|^[A-Z]{3}\d[A-Z]\d{2}$/',
-                        message: 'A placa deve estar no formato ABC-1234 ou ABC1D23 (Mercosul)'
-                    ),
-                ),
+                new Placa(),
             ],
             'marca' => [
                 new Assert\Optional(
