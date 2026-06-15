e01690b51ba8987e4c1e17dfccf3aff60b45539f
Oeslei Kuhn <oesleik@gmail.com>
2026-06-15 20:57:28 -0300

    Fix migration OS

--- src/migrations/260614100100_ordens_servico_table.sql
+++ src/migrations/260614100100_ordens_servico_table.sql
@@ -3,7 +3,7 @@ CREATE TABLE `ordens_servico` (
     `id_cliente`         INT UNSIGNED                                                                                    NOT NULL,
     `id_veiculo`         INT UNSIGNED                                                                                    NOT NULL,
     `situacao`           ENUM('Recebida', 'EmDiagnostico', 'AguardandoAprovacao', 'Aprovada', 'Rejeitada', 'EmExecucao', 'Finalizada', 'Entregue') NOT NULL DEFAULT 'Recebida',
-    `valor_total`        DECIMAL(10, 2),                                                                                 NOT NULL,
+    `valor_total`        DECIMAL(10, 2)                                                                                  NOT NULL,
     `data_solicitacao`   DATETIME                                                                                        NOT NULL,
     `data_aprovacao`     DATETIME,
     CONSTRAINT `fk_ordens_servico_cliente` FOREIGN KEY (`id_cliente`) REFERENCES `clientes` (`id`),
